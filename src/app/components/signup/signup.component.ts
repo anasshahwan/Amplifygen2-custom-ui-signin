cb879854c641e1c5db60f04198e5f68d59cb8117
Anas Shahwan <anas.shahwan20@gmail.com>
2025-09-04 17:32:29 -0400

    signup form created

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
-
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
+
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.scss'
+  styleUrl: './signup.component.scss',
 })
 export class SignupComponent {
+  signUpForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.signUpForm = this.fb.group({
+      name: ['', Validators.required],
+      companyName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.signUpForm.valid) {
+      console.log('Form Data:', this.signUpForm.value);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 }

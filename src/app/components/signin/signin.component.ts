229d7da4d8d9bbcf52908830df801f92fbe74048
Anas Shahwan <anas.shahwan20@gmail.com>
2025-09-04 17:40:48 -0400

    login form added

--- src/app/components/signin/signin.component.ts
+++ src/app/components/signin/signin.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
-
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-signin',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './signin.component.html',
-  styleUrl: './signin.component.scss'
+  styleUrl: './signin.component.scss',
 })
 export class SigninComponent {
+  signInForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.signInForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+  onSubmit(): void {
+    if (this.signInForm.valid) {
+      console.log('Form Data:', this.signInForm.value);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 }

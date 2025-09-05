3cdd27fdce7090d52af732c37d0a661ec591545a
Anas Shahwan <anas.shahwan20@gmail.com>
2025-09-05 17:29:49 -0400

    allow users to update their username

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,16 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-import { fetchUserAttributes, getCurrentUser, signOut } from 'aws-amplify/auth';
+import {
+  fetchUserAttributes,
+  getCurrentUser,
+  signOut,
+  updateUserAttributes,
+} from 'aws-amplify/auth';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
 })
 export class ProfileComponent {
   constructor(private router: Router) {}
   user: any = { email: '', name: '', companyName: '' };
+
+  isEditing: Boolean = false;
+  newUserName = '';
 
   async ngOnInit() {
     const res = await fetchUserAttributes();
@@ -22,5 +31,23 @@ export class ProfileComponent {
   async signUserOut() {
     await signOut();
     this.router.navigate(['/signin']);
+  }
+
+  edit() {
+    this.isEditing = true;
+
+    this.newUserName = this.user.name;
+  }
+  saveUserAttributes() {
+    this.isEditing = false;
+    this.updateUserAttr(this.newUserName);
+    this.user.name = this.newUserName;
+  }
+
+  async updateUserAttr(newUserName: string) {
+    const result = await updateUserAttributes({
+      userAttributes: { preferred_username: newUserName },
+    });
+    console.log(result);
   }
 }

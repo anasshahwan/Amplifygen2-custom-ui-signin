95e3f3caff4d0c52145cf101868e051762e558ab
Anas Shahwan <anas.shahwan20@gmail.com>
2025-09-05 17:08:07 -0400

    display user attributes

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { fetchUserAttributes, getCurrentUser } from 'aws-amplify/auth';
 
 @Component({
   selector: 'app-profile',
   imports: [],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+  styleUrl: './profile.component.scss',
 })
 export class ProfileComponent {
+  user: any = { email: '', name: '', companyName: '' };
 
+  async ngOnInit() {
+    const res = await fetchUserAttributes();
+    console.log(res);
+    this.user.email = res.email;
+    this.user.name = res.preferred_username;
+    this.user.companyName = res['custom:companyName'];
+  }
 }

b9ce7b0cc474f26d3b81a9c9d8be47a12c4eb37c
Anas Shahwan <anas.shahwan20@gmail.com>
2025-09-06 11:45:12 -0400

    Hub Events added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Hub } from 'aws-amplify/utils';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'amplifygen2-custom-ui-login';
+
+  ngOnInit() {
+    Hub.listen('auth', ({ payload }) => {
+      // console.log(payload.event);
+      switch (payload.event) {
+        case 'signedIn':
+          console.log('User signed in..');
+          break;
+        case 'signedOut':
+          console.log('User signed out..');
+          break;
+        case 'signInWithRedirect':
+          console.log('User signed in using third party provider.');
+          break;
+        default:
+          console.log('Unhandled auth event:', payload.event);
+      }
+    });
+  }
 }
